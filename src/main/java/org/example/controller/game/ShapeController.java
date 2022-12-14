a22477fc23f179f34444936bfadd870e061b3d62
Pedro Fonseca <pefonseca10@gmail.com>
2022-12-14 23:22:38 +0000

    Think I've got colision sorted out

--- src/main/java/org/example/controller/game/ShapeController.java
+++ src/main/java/org/example/controller/game/ShapeController.java
@@ -3,11 +3,15 @@ package org.example.controller.game;
 import org.example.MainGame;
 import org.example.gamestate.MenuState;
 import org.example.gui.GUI;
+import org.example.model.Position;
 import org.example.model.game.entities.Block;
+import org.example.model.game.entities.Shape;
 import org.example.model.game.window.Window;
 import org.example.model.menu.Menu;
 
 import java.io.IOException;
+
+import static java.lang.Math.abs;
 
 public class ShapeController extends GameController{
     public ShapeController(Window window) {
@@ -33,19 +37,39 @@ public class ShapeController extends GameController{
     }
 
     private void rotateShape() {
+        Position originalPosition = new Position(getModel().getPlayingShape().getPosition().getX(), getModel().getPlayingShape().getPosition().getY());
         getModel().getPlayingShape().rotateShape();
         getModel().getPlayingShape().updateShape();
-        for (int j = 0; j < 5; j++) {
+        rotateShapeCollision(originalPosition, getModel().getPlayingShape());
+    }
+
+    private void rotateShapeCollision(Position originalPosition, Shape shape) {
+        for (int j = 0; j < 2; j++) {
             for (int i = 0; i < 5; i++) {
-                int tries = 0;
-                while (getModel().collisionImminent(getModel().getPlayingShape().getBlocks().get(i).getPosition(), "own") && tries < 3) {
-                    if (getModel().getPlayingShape().getBlocks().get(i).getPosition().getY() == 26) getModel().getPlayingShape().pushShapeUp();
-                    else if (getModel().getPlayingShape().getBlocks().get(i).getPosition().getX() < 15) getModel().getPlayingShape().pushShapeRight();
-                    else getModel().getPlayingShape().pushShapeLeft();
-                    tries++;
+                while (getModel().collisionImminent(shape.getBlocks().get(i).getPosition(), "own")) {
+                    if (shape.getBlocks().get(i).getPosition().getY() == 26)
+                        shape.pushShapeUp();
+                    else if (shape.getBlocks().get(i).getPosition().getX() < shape.getPosition().getX())
+                        shape.pushShapeRight();
+                    else shape.pushShapeLeft();
                 }
             }
+            if (abs(originalPosition.getX()-shape.getPosition().getX()) > 4) {
+                shape.setPosition(originalPosition);
+                shape.updateShape();
+            }
         }
+
+        for (int i = 0; i < 5; i++) {
+            if (getModel().collisionImminent(shape.getBlocks().get(i).getPosition(), "own")) {
+                shape.pushShapeUp();
+                Position position = new Position(originalPosition.getX(), originalPosition.getY());
+                rotateShapeCollision(position, shape);
+                break;
+            }
+        }
+
+
     }
 
     private void pushShapeDown() {
